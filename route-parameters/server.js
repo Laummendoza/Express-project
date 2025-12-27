49a1211de11ac4bb97d22a920346272c00248349
Lau Mendoza <laumendoza@MacBook-Pro-de-Laura.local>
2025-12-27 18:22:36 -0300

    Implementa Handlebars para renderizar vistas desde Express

--- route-parameters/server.js
+++ route-parameters/server.js
@@ -6,6 +6,8 @@ const friendsRouter = require("./routes/friendsRouter");
 const messagesRouter = require("./routes/messagesRouter");
 
 const app = express();
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname,'views'));
 const PORT = process.env.PORT || 3000;
 
 
@@ -19,6 +21,13 @@ console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 
 app.use('/site',express.static(path.join(__dirname,'public')));
 app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.render('index',{
+        title: "My web site",
+        caption: "Bienvenidos a mi sitio web con Express y Node.js"
+    });
+});
 
 
 app.use("/friends", friendsRouter);
